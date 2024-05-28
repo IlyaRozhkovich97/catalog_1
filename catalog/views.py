a26a4a2a57c86b0c1df80c13b59a575312e82007
Илья Рожкович <rozhkovich.ilya97@gmail.com>
2024-05-28 16:47:31 +0300

    вторая фиксация

--- catalog/views.py
+++ catalog/views.py
@@ -12,6 +12,11 @@ from catalog.models import Product
 class HomePageView(TemplateView):
     template_name = 'catalog/home_page.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['products'] = Product.objects.filter(is_published=True)
+        return context
+
 
 class ContactInformationView(TemplateView):
     template_name = 'catalog/contact_information.html'
