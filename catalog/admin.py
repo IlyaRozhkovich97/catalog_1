cac3271254a0098f97d62947ccaf11ffa7ff8605
Илья Рожкович <rozhkovich.ilya97@gmail.com>
2024-06-07 20:45:33 +0300

    16 фиксация

--- catalog/admin.py
+++ catalog/admin.py
@@ -12,9 +12,3 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ("id", "name", "category", "purchase_price")
     list_filter = ("category",)
     search_fields = ("name",)
-
-@admin.register(Version)
-class VersionAdmin(admin.ModelAdmin):
-    list_display = ("id", "product", "version_number", "version_name", "is_current")
-    list_filter = ("product", "is_current")
-    search_fields = ("version_name", "version_number")
--- config/settings.py
+++ config/settings.py
@@ -85,13 +85,16 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
+LANGUAGE_CODE = 'ru'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
-
+USE_L10N = True
 USE_TZ = True
+
+LOCALE_PATHS = [
+    os.path.join(BASE_DIR, 'locale'),
+]
 
 
 STATIC_URL = 'static/'
